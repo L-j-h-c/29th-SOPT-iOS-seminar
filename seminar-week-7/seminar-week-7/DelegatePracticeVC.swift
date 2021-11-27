217cb843406b2c326c193e4d63aadbc35cdf3ac8
L-j-h-c <ckrgkswnsgh@naver.com>
2021-11-27 15:30:09 +0900

    ✨[FEAT] : 데이터전달 연습
    notification center
    segue
    present를 이용한 데이터 전달

---
+++ seminar-week-7/seminar-week-7/DelegatePracticeVC.swift
@@ -0,0 +1,29 @@
+//
+//  DelegatePracticeVC.swift
+//  seminar-week-7
+//
+//  Created by Junho Lee on 2021/11/27.
+//
+
+import UIKit
+
+class DelegatePracticeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ seminar-week-7/seminar-week-7/keywordCVC.swift
@@ -0,0 +1,17 @@
+//
+//  keywordCVC.swift
+//  seminar-week-7
+//
+//  Created by Junho Lee on 2021/11/27.
+//
+
+import UIKit
+
+class keywordCVC: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
